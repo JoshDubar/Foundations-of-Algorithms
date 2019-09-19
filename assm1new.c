cab9e107c9bb3bc123b6db3dc98fb14b6eea128a
JoshDubar <joshuadubar123@gmail.com>
2019-09-19 22:11:50 +1000

    part 2 done MAIN REDUCED

--- assm1new.c
+++ assm1new.c
@@ -1,5 +1,7 @@
 /* Solution to comp10002 Assignment 1, 2019 semester 2.
+
    Authorship Declaration:
+
    (1) I certify that the program contained in this submission is completely
    my own individual work, except where explicitly noted by comments that
    provide details otherwise.  I understand that work that has been developed
@@ -11,11 +13,13 @@
    may be penalized by mark deductions, or by other penalties determined
    via the University of Melbourne Academic Honesty Policy, as described
    at https://academicintegrity.unimelb.edu.au.
+
    (2) I also certify that I have not provided a copy of this work in either
    softcopy or hardcopy or any other form to any other student, and nor will
    I do so until after the marks are released. I understand that providing
    my work to other students, regardless of my intention or any undertakings
    made to me by that other student, is also Academic Misconduct.
+
    (3) I further understand that providing a copy of the assignment
    specification to any form of code authoring or assignment tutoring
    service, or drawing the attention of others to such services and code
@@ -25,8 +29,10 @@
    I understand that an allegation of Student General Misconduct may arise
    regardless of whether or not I personally make use of such solutions
    or sought benefit from such actions.
+
    Signed by: Joshua Philip Dubar 1086080
    Dated:     9/09/2019
+
 */
 /*Algorithms are Fun! */
 #include <stdio.h>
@@ -73,29 +79,6 @@ main(int argc, char *argv[]) {
     /* read in each line and send them off for processing*/
     while (fgets(line, sizeof line, stdin)) {
         process_line(&count, &previous, &margin, &width, line);
-        if (line[0] == FULLSTOP) {            
-            if (line[1] == LEFT && previous) {
-                margin = nn(line);
-            } else if (line[1] == WIDTH && previous) {
-                width = nn(line);
-            } else if (line[1] == LEFT && !previous) {
-                margin = check_commands(line);
-            } else if (line[1] == WIDTH && !previous) {
-                width = check_commands(line);
-            } else if ((line[1] == BREAK || line[1] == PARAGRAPH) && !previous) {
-                check_commands(line);
-            } else if ((line[1] == BREAK || line[1] == PARAGRAPH) && previous) {
-                continue;
-            }
-            previous = 1; 
-            count = 0;
-        } else if (line[0] != FULLSTOP) {
-            if (previous) {
-                print_spaces(margin);
-            }
-            remove_spaces(line, margin, width);  
-            previous = 0;   
-        }
     }
     return 0;
 }
@@ -152,7 +135,7 @@ int check_commands(char *line) {
     /* if .l or .w we want to change the margin size/width, so we return a number */
     } else if (line[1] == LEFT) {
         process_paragraph();
-        int i = nn(line);
+        int i =  nn(line);
         return i;
     } else if (line[1] == WIDTH) {
         process_paragraph();
